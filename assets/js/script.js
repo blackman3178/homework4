a14e8fb55712ef8bfbd43e32b46c2933f0dc8de0
Noah Blackman <nablackm@usc.edu>
2021-09-20 23:45:13 -0400

    commit with start of startQuiz fxn

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var startButton = document.querySelector("#startQuiz");
 var timerElement = document.querySelector("#time");
+var listEl = document.querySelector("#question-list");
 
 //^^ declare initial variables from HTML
 
@@ -24,15 +25,39 @@ var question4 = new Question("String values must be enclosed within ___ when bei
 
 var question5 = new Question("A very useful tool used during the development & debugging for printing content to the debugger is: ", "JavaScript,terminal/bash,for loops,console log".split(","), 3);
 
-console.log(question5);
+var questionArray = [question1, question2, question3, question4, question5];
+
+console.log(questionArray);
 
 
-// function to clear the startpage
+// function to clear the startpage and starts the questions 
 function clearStartPage() {
-    var quizPage = document.querySelectorAll(".start-page");
-    for (var i = 0; i < quizPage.length; i++) {
-        quizPage[i].setAttribute("style", "visibility: hidden;");
+    var startPage = document.querySelectorAll(".start-page");
+    for (var i = 0; i < startPage.length; i++) {
+        startPage[i].setAttribute("style", "visibility: hidden;");
     }
+    askQuestions();
 }
-// clears start page when the start quiz button is clicked.;\
+// clears start page when the start quiz button is clicked.;
 startButton.addEventListener("click",clearStartPage);
+
+function startQuiz() {
+    var i = 0;
+    var pTag = document.querySelector("#prompt");
+    var quizPage = document.querySelectorAll("#hiddenByDefault");
+
+    //shows the elements hidden by default 
+    for ( var j = 0; j < quizPage.length; j ++) {
+        quizPage[j].setAttribute("style", "visibility: visible;");
+    }
+
+    pTag.innerHTML = "hi there";
+    // gets the string variables from the array within the questions object and appends them to the ol element in the HTML
+    // while(i < questionArray.length) {
+    //     pTag.innerHTML = questionArray[i].prompt;
+    //     for ( var n = 0; n < questionArray[i].answers.length; n ++) {
+    //         var li = document.createElement("li");
+    //         listEl.appendChild(li);
+    //     }
+    // }
+}
